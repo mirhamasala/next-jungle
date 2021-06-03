fd1513cae653540fb33d27ef51ee803d1e899ff0
mirhamasala <mirha.masala@me.com>
2021-06-03 10:50:12 +0200

    Include typography

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "../styles/globals.scss";
+
+function MyApp({Component, pageProps}) {
+  return (
+    <>
+      <Head>
+        <title>Welcome to the Jungle</title>
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300;400&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
