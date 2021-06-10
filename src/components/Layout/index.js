89bfad97010c20f21d920b39e3c48db22c492013
Mirha Masala <mirha.masala@me.com>
2021-06-10 10:49:47 +0200

    Add layout section styles

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,17 +4,27 @@ import Header from "../../components/Header";
 
 import styles from "./index.module.scss";
 
-const Layout = ({header, main}) => {
+const Layout = ({ header, main, sections }) => {
   return (
     <div className={styles.grid}>
       <Header className={styles.header}>{header}</Header>
-      <main className={styles.main}>{main}</main>
+      <main className={styles.main}>
+        {main && main}
+        {sections &&
+          sections.map((section, index) => (
+            <section key={index} className={styles.section}>
+              {section}
+            </section>
+          ))}
+      </main>
     </div>
   );
 };
 
-// Layout.propTypes = {
-//   children: PropTypes.node.isRequired
-// };
+Layout.propTypes = {
+  header: PropTypes.string.isRequired,
+  main: PropTypes.node,
+  sections: PropTypes.node,
+};
 
 export default Layout;
