3789bf490665c1acee21a93cf1f85faa9459782d
Mirha Masala <mirha.masala@me.com>
2021-06-10 14:33:07 +0200

    Include StickyHeader

--- src/pages/css-snippets/index.js
+++ src/pages/css-snippets/index.js
@@ -1,14 +1,8 @@
 import HorizontalOverflow from "../../components/_css-snippets/HorizontalOverflow";
+import StickyHeader from "../../components/_css-snippets/StickyHeader";
 import StickySections from "../../components/_css-snippets/StickySections";
 
 import Layout from "../../components/Layout";
-
-const stickySections = (
-  <>
-    <h2>Sticky Sections</h2>
-    <StickySections />
-  </>
-);
 
 const horizontalOverflow = (
   <>
@@ -17,7 +11,22 @@ const horizontalOverflow = (
   </>
 );
 
-const sections = [stickySections, horizontalOverflow];
+const stickyHeader = (
+  <>
+    <h2>Sticky Header</h2>
+    <StickyHeader />
+  </>
+);
+
+const stickySections = (
+  <>
+    <h2>Sticky Sections</h2>
+    <StickySections />
+  </>
+);
+
+/* Order matters */
+const sections = [stickySections, horizontalOverflow, stickyHeader];
 
 export default function CssSnippets() {
   return <Layout header={"CSS Snippets"} sections={sections} />;
