d748703169e8ee60d0d9f909ea56e9a433f85760
Mirha Masala <mirha.masala@me.com>
2021-06-10 10:50:22 +0200

    Include sections

--- src/pages/css-snippets/index.js
+++ src/pages/css-snippets/index.js
@@ -1,18 +1,24 @@
+import HorizontalOverflow from "../../components/_css-snippets/HorizontalOverflow";
 import StickySections from "../../components/_css-snippets/StickySections";
 
 import Layout from "../../components/Layout";
-import Title from "../../components/Title";
+
+const stickySections = (
+  <>
+    <h2>Sticky Sections</h2>
+    <StickySections />
+  </>
+);
+
+const horizontalOverflow = (
+  <>
+    <h2>Horizontal Overflow</h2>
+    <HorizontalOverflow />
+  </>
+);
+
+const sections = [stickySections, horizontalOverflow];
 
 export default function CssSnippets() {
-  return (
-    <Layout
-      header={"CSS Snippets"}
-      main={
-        <div>
-          <Title color="pink">Sticky Sections</Title>
-          <StickySections />
-        </div>
-      }
-    />
-  );
+  return <Layout header={"CSS Snippets"} sections={sections} />;
 }
