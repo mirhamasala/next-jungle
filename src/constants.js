068f48555ab8ffc800bb50b290203ae8441cd773
Mirha Masala <mirha.masala@me.com>
2021-06-10 14:31:23 +0200

    Add default texts

--- src/constants.js
+++ src/constants.js
@@ -1,35 +1,47 @@
 export const DEFAULT_SECTIONS = [
   {
     title: "Chapter 1",
-    text: "I'm activated charcoal bicycle rights craft beer, humblebrag viral leggings paleo yuccie austin. Slow-carb cloud bread hella, poke freegan sriracha celiac franzen. Authentic VHS glossier master cleanse, disrupt waistcoat messenger bag selfies blog iceland. Glossier poke tacos keytar YOLO cloud bread. Narwhal palo santo waistcoat pop-up slow-carb. DIY farm-to-table enamel pin paleo, cornhole trust fund heirloom la croix banh mi tumblr asymmetrical 90's etsy meditation. Glossier selfies migas seitan."
+    text:
+      "I'm activated charcoal bicycle rights craft beer, humblebrag viral leggings paleo yuccie austin. Slow-carb cloud bread hella, poke freegan sriracha celiac franzen. Authentic VHS glossier master cleanse, disrupt waistcoat messenger bag selfies blog iceland. Glossier poke tacos keytar YOLO cloud bread. Narwhal palo santo waistcoat pop-up slow-carb. DIY farm-to-table enamel pin paleo, cornhole trust fund heirloom la croix banh mi tumblr asymmetrical 90's etsy meditation. Glossier selfies migas seitan.",
   },
   {
     title: "Chapter 2",
-    text: "Glossier poke tacos keytar YOLO cloud bread. Narwhal palo santo waistcoat pop-up slow-carb. DIY farm-to-table enamel pin paleo, cornhole trust fund heirloom la croix banh mi tumblr asymmetrical 90's etsy meditation. Glossier selfies migas seitan. Umami echo park vape whatever cray skateboard. Blue bottle etsy artisan food truck narwhal tote bag chambray. Af occupy vexillologist flannel asymmetrical man bun health goth glossier."
+    text:
+      "Glossier poke tacos keytar YOLO cloud bread. Narwhal palo santo waistcoat pop-up slow-carb. DIY farm-to-table enamel pin paleo, cornhole trust fund heirloom la croix banh mi tumblr asymmetrical 90's etsy meditation. Glossier selfies migas seitan. Umami echo park vape whatever cray skateboard. Blue bottle etsy artisan food truck narwhal tote bag chambray. Af occupy vexillologist flannel asymmetrical man bun health goth glossier.",
   },
   {
     title: "Chapter 3",
-    text: "Umami echo park vape whatever cray skateboard. Blue bottle etsy artisan food truck narwhal tote bag chambray. Af occupy vexillologist flannel asymmetrical man bun health goth glossier. I'm activated charcoal bicycle rights craft beer, humblebrag viral leggings paleo yuccie austin. Slow-carb cloud bread hella, poke freegan sriracha celiac franzen. Authentic VHS glossier master cleanse, disrupt waistcoat messenger bag selfies blog iceland."
-  }
+    text:
+      "Umami echo park vape whatever cray skateboard. Blue bottle etsy artisan food truck narwhal tote bag chambray. Af occupy vexillologist flannel asymmetrical man bun health goth glossier. I'm activated charcoal bicycle rights craft beer, humblebrag viral leggings paleo yuccie austin. Slow-carb cloud bread hella, poke freegan sriracha celiac franzen. Authentic VHS glossier master cleanse, disrupt waistcoat messenger bag selfies blog iceland.",
+  },
 ];
 
 export const defaultTexts = [
   "I'm activated charcoal bicycle rights craft beer, humblebrag viral leggings paleo yuccie austin. Slow-carb cloud bread hella, poke freegan sriracha celiac franzen. Authentic VHS glossier master cleanse, disrupt waistcoat messenger bag selfies blog iceland.",
   "Glossier poke tacos keytar YOLO cloud bread. Narwhal palo santo waistcoat pop-up slow-carb. DIY farm-to-table enamel pin paleo, cornhole trust fund heirloom la croix banh mi tumblr asymmetrical 90's etsy meditation. Glossier selfies migas seitan.",
-  "Umami echo park vape whatever cray skateboard. Blue bottle etsy artisan food truck narwhal tote bag chambray. Af occupy vexillologist flannel asymmetrical man bun health goth glossier."
+  "Umami echo park vape whatever cray skateboard. Blue bottle etsy artisan food truck narwhal tote bag chambray. Af occupy vexillologist flannel asymmetrical man bun health goth glossier.",
 ];
 
 export const defaultSections = [
   {
     title: "Section 1",
-    text: "I'm activated charcoal bicycle rights craft beer, humblebrag viral leggings paleo yuccie austin. Slow-carb cloud bread hella, poke freegan sriracha celiac franzen. Authentic VHS glossier master cleanse, disrupt waistcoat messenger bag selfies blog iceland."
+    text:
+      "I'm activated charcoal bicycle rights craft beer, humblebrag viral leggings paleo yuccie austin. Slow-carb cloud bread hella, poke freegan sriracha celiac franzen. Authentic VHS glossier master cleanse, disrupt waistcoat messenger bag selfies blog iceland.",
   },
   {
     title: "Section 2",
-    text: "Glossier poke tacos keytar YOLO cloud bread. Narwhal palo santo waistcoat pop-up slow-carb. DIY farm-to-table enamel pin paleo, cornhole trust fund heirloom la croix banh mi tumblr asymmetrical 90's etsy meditation. Glossier selfies migas seitan."
+    text:
+      "Glossier poke tacos keytar YOLO cloud bread. Narwhal palo santo waistcoat pop-up slow-carb. DIY farm-to-table enamel pin paleo, cornhole trust fund heirloom la croix banh mi tumblr asymmetrical 90's etsy meditation. Glossier selfies migas seitan.",
   },
   {
     title: "Section 3",
-    text: "Umami echo park vape whatever cray skateboard. Blue bottle etsy artisan food truck narwhal tote bag chambray. Af occupy vexillologist flannel asymmetrical man bun health goth glossier."
-  }
+    text:
+      "Umami echo park vape whatever cray skateboard. Blue bottle etsy artisan food truck narwhal tote bag chambray. Af occupy vexillologist flannel asymmetrical man bun health goth glossier.",
+  },
+];
+
+export const DEFAULT_TEXTS = [
+  "I'm activated charcoal bicycle rights craft beer, humblebrag viral leggings paleo yuccie austin. Slow-carb cloud bread hella, poke freegan sriracha celiac franzen. Authentic VHS glossier master cleanse, disrupt waistcoat messenger bag selfies blog iceland.",
+  "Glossier poke tacos keytar YOLO cloud bread. Narwhal palo santo waistcoat pop-up slow-carb. DIY farm-to-table enamel pin paleo, cornhole trust fund heirloom la croix banh mi tumblr asymmetrical 90's etsy meditation. Glossier selfies migas seitan.",
+  "Umami echo park vape whatever cray skateboard. Blue bottle etsy artisan food truck narwhal tote bag chambray. Af occupy vexillologist flannel asymmetrical man bun health goth glossier.",
 ];
