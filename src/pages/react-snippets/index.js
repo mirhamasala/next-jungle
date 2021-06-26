4fae1f7f538905eac93c5f31497cd3719d649fed
Mirha Masala <mirha.masala@me.com>
2021-06-26 16:26:11 +0100

    Include RainbowNav

--- src/pages/react-snippets/index.js
+++ src/pages/react-snippets/index.js
@@ -1,7 +1,19 @@
+import RainbowNav from "../../components/_react-snippets/RainbowNav";
+
 import Layout from "../../components/Layout";
 
+// import styles from "./index.module.scss";
+
+const rainbowNav = (
+  <>
+    <h2>Rainbow Nav</h2>
+    <RainbowNav />
+  </>
+);
+
+/* Order matters */
+const sections = [rainbowNav];
+
 export default function ReactSnippets() {
-  return (
-    <Layout header={"React Snippets"} main={<div>Hello React Snippets</div>} />
-  );
+  return <Layout header={"React Snippets"} sections={sections} />;
 }
